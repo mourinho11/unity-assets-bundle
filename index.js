3fd04159ed745f3cd3ecea77814893cca10ac541
mourinho11 <marouen.khadhar@gmail.com>
2022-10-04 13:02:23 +0100

    fix : project structure

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
 var cors = require('cors');
-
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
-app.get('/uploads/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, `./assets/${req.params.id}`));
-  });
-  
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(cors());
 
-app.listen(3001, (err) => {
+app.get('/bundles/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, `./assets/${req.params.id}`));
+    //res.send("dsdsdsdsdsdsdsdssdsdsdsd");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`Listening on PORT ${3001}`);
+    console.log(`Listening on PORT ${PORT}`);
   }
 });
